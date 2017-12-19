b8df768993889443c3f1778961c9a312180469ad
Kenold Beauplan <bkenold@gmail.com>
2017-12-19 09:41:55 -0500

    add new masonry script

--- js/moments.js
+++ js/moments.js
@@ -1,6 +1,24 @@
-var $grid = $('.grid').imagesLoaded( function() {
-  // init Masonry after all images have loaded
-  $grid.masonry({
-    // options...
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    columnWidth: '.grid-sizer'
+  }
+});
+// layout Isotope after each image loads
+$grid.imagesLoaded().progress(function () {
+  $grid.isotope('layout');
+});
+
+// Isotope click function
+$('.iso-nav ul li').click(function () {
+  $('.iso-nav ul li').removeClass('active');
+  $(this).addClass('active');
+
+  var selector = $(this).attr('data-filter');
+  $('.grid').isotope({
+    filter: selector
   });
+  return false;
 });
